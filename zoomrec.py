3a1761821b9ba71a239c546d518feda957b4b9b9
root <josia-login@rosskopfs.de>
2022-01-14 13:19:16 +0100

    reduce polling

--- zoomrec.py
+++ zoomrec.py
@@ -933,7 +933,7 @@ if __name__ == '__main__':
 
 while True:
     schedule.run_pending()
-    time.sleep(1)
+    time.sleep(30)
     time_of_next_run = schedule.next_run()
     time_now = datetime.now()
     remaining = time_of_next_run - time_now
